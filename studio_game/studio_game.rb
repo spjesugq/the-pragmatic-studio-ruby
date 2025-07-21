a362a3bf99c67258b66835c6070836545a9724b2
spjesugq <jesus.gonzalez@simplepractice.com>
2025-07-21 15:25:43 -0600

    Remove comments from studio_game.rb

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -3,11 +3,6 @@ puts emoji * 25
 
 puts "\nLet's play a game!\n\n\t3\n\t2\n\t1\n\n"
 
-# name = "finn"
-# health = 60
-
-# puts "#{name.capitalize}'s health is #{health}"
-
 def say_hello(name:, health: 100)
   "I'm #{name} with a health of #{health}"
 end
