5b65cf306a32a3ddce7e17a024f69e9a32e22b7d
spjesugq <jesus.gonzalez@simplepractice.com>
2025-07-21 12:45:38 -0600

    Add Objects Everywhere studio_game

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -3,8 +3,19 @@ puts emoji * 25
 
 puts "\nLet's play a game!\n\n\t3\n\t2\n\t1\n\n"
 
-
-name = "Finn"
+name = "finn"
 health = 60
 
-puts "#{name}'s health is #{health}"
+puts "#{name.capitalize}'s health is #{health}"
+# puts "#{name.upcase}'s health is #{health}"
+# puts "#{name.capitalize}'s health is #{health}".center(50, '*')
+# puts "#{name.capitalize}'s health is #{health}"
+# puts "#{name.capitalize.ljust(30, ".")} #{health} health"
+
+# name = "  \n finn \t "
+# name.strip!
+# puts "#{name.capitalize}'s health is #{health}"
+
+# puts "#{name.capitalize}'s health is #{health.to_s.reverse}"
+# puts "#{name.capitalize}'s health is #{health.to_s.reverse.to_i}"
+# puts "#{name.capitalize}'s health is #{health ** 2}"
