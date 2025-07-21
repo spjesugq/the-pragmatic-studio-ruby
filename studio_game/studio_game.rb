b2d983bdb31af34142d09761a15439244a1dd42f
spjesugq <jesus.gonzalez@simplepractice.com>
2025-07-21 11:32:16 -0600

    Add ABCs and 123s studio_game

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -1,4 +1,13 @@
-puts "Let's play a game!"
-
 emoji = "🚀"
 puts emoji * 25
+
+puts "\nLet's play a game!\n\n\t3\n\t2\n\t1\n\n"
+
+
+name = "Finn"
+health = 60
+
+puts "#{name}'s health is #{health}"
+# puts "#{name}'s health is #{health * 3}"
+# puts "#{name}'s health is #{health / 9.0}"
+# puts "#{name}'s health is #{health / 9}"
