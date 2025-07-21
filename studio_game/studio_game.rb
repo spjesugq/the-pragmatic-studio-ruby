17ecc97f807db0911b84d5a9e8f14f74fc89e84e
spjesugq <jesus.gonzalez@simplepractice.com>
2025-07-21 16:09:10 -0600

    Add Attr Is For Attribute studio_game

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -1,4 +1,7 @@
 class Player
+  attr_accessor :name
+  attr_reader :health
+
   def initialize(name, health = 100)
     @name = name.capitalize
     @health = health
@@ -12,15 +15,21 @@ class Player
     @health += 20
   end
 
+  def score
+    @health + @name.length
+  end
+
+  def name=(new_name)
+    @name = new_name.capitalize
+  end
+
   def to_s
-    "I'm #{@name} with a health of #{@health}"
+    "I'm #{@name} with a health of #{@health} and a score of #{@score}"
   end
 end
 
 player_1 = Player.new("finn", 60)
-puts player_1.inspect
-puts player_1
-player_1.drain
-puts player_1
-player_1.boost
+puts player_1.health
+player_1.name = "alexander"
+puts player_1.name
 puts player_1
