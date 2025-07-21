45cafb65cb413ae2dc3277307873b07a816db485
spjesugq <jesus.gonzalez@simplepractice.com>
2025-07-21 12:50:10 -0600

    Remove comments from studio_game.rb

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -7,15 +7,3 @@ name = "finn"
 health = 60
 
 puts "#{name.capitalize}'s health is #{health}"
-# puts "#{name.upcase}'s health is #{health}"
-# puts "#{name.capitalize}'s health is #{health}".center(50, '*')
-# puts "#{name.capitalize}'s health is #{health}"
-# puts "#{name.capitalize.ljust(30, ".")} #{health} health"
-
-# name = "  \n finn \t "
-# name.strip!
-# puts "#{name.capitalize}'s health is #{health}"
-
-# puts "#{name.capitalize}'s health is #{health.to_s.reverse}"
-# puts "#{name.capitalize}'s health is #{health.to_s.reverse.to_i}"
-# puts "#{name.capitalize}'s health is #{health ** 2}"
