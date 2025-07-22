e2ce5066c59944df3eed15e3bc0c8c27f8a05d7f
spjesugq <jesus.gonzalez@simplepractice.com>
2025-07-22 10:28:52 -0600

    Add Organizing Files flicks

---
+++ 14-organizing-files/flicks/lib/flicks/playlist.rb
@@ -0,0 +1,45 @@
+class Playlist
+  attr_reader :name, :movies
+
+  def initialize(name)
+    puts self.inspect
+    @name = name
+    puts self.inspect
+    @movies = []
+    puts self.inspect
+  end
+
+  def add_movie(movie)
+    @movies << movie
+  end
+
+  def roll_die
+    rand(1..6)
+  end
+
+  def play
+    puts "*" * 30
+    puts "#{@name}'s playlist:"
+
+    puts "\nBefore watching:"
+    puts @movies
+    
+    puts ""
+    @movies.each do |movie|
+      case roll_die
+      when 1..2
+        movie.thumbs_down
+        puts "#{movie.title} got a 👎"
+      when 3..4
+        puts "#{movie.title} got skipped"
+      else
+        movie.thumbs_up
+        puts "#{movie.title} got a 👍"
+      end
+    end
+
+    puts "\nAfter watching:"
+    puts @movies
+    puts ""
+  end
+end
