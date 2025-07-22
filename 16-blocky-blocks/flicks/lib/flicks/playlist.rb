2b348e551a58894a64f9c1556214f40ebf88f547
spjesugq <jesus.gonzalez@simplepractice.com>
2025-07-22 14:54:55 -0600

    Add Blocky Blocks flicks

---
+++ 16-blocky-blocks/flicks/lib/flicks/playlist.rb
@@ -0,0 +1,44 @@
+class Playlist
+  attr_reader :name, :movies
+
+  def initialize(name)
+    @name = name
+    @movies = []
+  end
+
+  def add_movie(movie)
+    @movies << movie
+  end
+
+  def roll_die
+    rand(1..6)
+  end
+
+  def play(viewings = 3)
+    puts "*" * 30
+    puts "#{@name}'s playlist:"
+
+    puts "\nBefore watching:"
+    puts @movies
+    
+    1.upto(viewings) do |viewing_number|
+      puts "\nViewing #{viewing_number}:"
+      @movies.each do |movie|
+        case roll_die
+        when 1..2
+          movie.thumbs_down
+          puts "#{movie.title} got a 👎"
+        when 3..4
+          puts "#{movie.title} got skipped"
+        else
+          movie.thumbs_up
+          puts "#{movie.title} got a 👍"
+        end
+      end
+    end
+
+    puts "\nAfter watching:"
+    puts @movies
+    puts ""
+  end
+end
---
+++ 16-blocky-blocks/flicks/scratch.rb
@@ -0,0 +1,17 @@
+# do
+#   puts "Star Wars"
+# end
+
+# 9.times do
+#   puts "Star Wars"
+# end
+
+# 9.times do |episode|
+#   puts "Star Wars: Episode #{episode}"
+# end
+
+# 1.upto(9) do |episode|
+#   puts "Star Wars: Episode #{episode}"
+# end
+
+1.upto(9) { |episode| puts "Star Wars: Episode #{episode}" }
