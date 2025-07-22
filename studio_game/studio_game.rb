de0008580bac5554e536167fb07bb5813b0e9157
spjesugq <jesus.gonzalez@simplepractice.com>
2025-07-22 09:37:44 -0600

    Add Array Party flicks

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -24,20 +24,36 @@ class Player
   end
 
   def to_s
-    "I'm #{@name} with a health of #{@health} and a score of #{@score}"
+    "I'm #{@name} with a health of #{@health} and a score of #{score}"
   end
 end
 
 player_1 = Player.new("finn", 60)
-number_rolled = rand(1..6)
+player_2 = Player.new("lucy", 90)
+player_3 = Player.new("jase")
+player_4 = Player.new("alex", 125)
+players = [player_1, player_2, player_3, player_4]
+players.pop
+player_5 = Player.new("cole", 75)
+players.push(player_5)
 
-case number_rolled
-when 1..2
-  player_1.drain
-  puts "#{player_1.name} got drained 😩"
-when 3..4
-  puts "#{player_1.name} got skipped"
-else
-  player_1.boost
-  puts "#{player_1.name} got boosted 😁"
+puts "Before playing:"
+puts players
+
+players.each do |player|
+  number_rolled = rand(1..6)
+  case number_rolled
+  when 1..2
+    player.drain
+    puts "#{player.name} got drained 😩"
+  when 3..4
+    puts "#{player.name} got skipped"
+  else
+    player.boost
+    puts "#{player.name} got boosted 😁"
+  end
 end
+puts ""
+
+puts "After playing:"
+puts players
