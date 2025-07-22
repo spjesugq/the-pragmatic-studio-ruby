1fbf580454a6599562ac7c005789d2a77377c949
spjesugq <jesus.gonzalez@simplepractice.com>
2025-07-22 09:59:36 -0600

    Add Objects Collaborating studio_game

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -28,6 +28,48 @@ class Player
   end
 end
 
+class Game
+  attr_reader :title, :players
+
+  def initialize(title)
+    @title = title
+    @players = []
+  end
+
+  def add_player(player)
+    @players << player
+  end
+
+  def roll_die
+    rand(1..6)
+  end
+
+  def play
+    puts "\nLet's play #{@title}!"
+
+    puts "\nBefore playing:"
+    puts @players
+
+    puts ""
+    @players.each do |player|
+      number_rolled = roll_die
+      case number_rolled
+      when 1..2
+        player.drain
+        puts "#{player.name} got drained 😩"
+      when 3..4
+        puts "#{player.name} got skipped"
+      else
+        player.boost
+        puts "#{player.name} got boosted 😁"
+      end
+    end
+
+    puts "\nAfter playing:"
+    puts @players
+  end
+end
+
 player_1 = Player.new("finn", 60)
 player_2 = Player.new("lucy", 90)
 player_3 = Player.new("jase")
@@ -37,23 +79,9 @@ players.pop
 player_5 = Player.new("cole", 75)
 players.push(player_5)
 
-puts "Before playing:"
-puts players
-
-players.each do |player|
-  number_rolled = rand(1..6)
-  case number_rolled
-  when 1..2
-    player.drain
-    puts "#{player.name} got drained 😩"
-  when 3..4
-    puts "#{player.name} got skipped"
-  else
-    player.boost
-    puts "#{player.name} got boosted 😁"
-  end
-end
-puts ""
-
-puts "After playing:"
-puts players
+game = Game.new("Winner Takes All")
+game.add_player(player_1)
+game.add_player(player_2)
+game.add_player(player_3)
+game.add_player(player_4)
+game.play
