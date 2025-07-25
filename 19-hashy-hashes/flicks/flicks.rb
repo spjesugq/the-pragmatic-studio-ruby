da1df1b353e49c49ef5f23e096ebab04314b0f17
spjesugq <jesus.gonzalez@simplepractice.com>
2025-07-25 11:52:13 -0600

    Add Hashy Hashes flicks

---
+++ 19-hashy-hashes/flicks/flicks.rb
@@ -0,0 +1,18 @@
+require_relative "lib/flicks/movie"
+require_relative "lib/flicks/playlist"
+
+movie_1 = Movie.new("goonies", 10)
+movie_2 = Movie.new("ghostbusters", 9)
+movie_3 = Movie.new("goldfinger")
+movie_4 = Movie.new("gremlines", 7)
+
+playlist_1 = Playlist.new("Kermit")
+playlist_1.add_movie(movie_1)
+playlist_1.add_movie(movie_2)
+playlist_1.add_movie(movie_3)
+playlist_1.play(1)
+
+playlist_2 = Playlist.new("Fozzie")
+playlist_2.add_movie(movie_3)
+playlist_2.add_movie(movie_4)
+playlist_2.play(20)
---
+++ 19-hashy-hashes/flicks/scratch.rb
@@ -0,0 +1,10 @@
+# snacks_eaten = {}
+snacks_eaten = Hash.new(0)
+
+snacks_eaten["popcorn"] = 3
+snacks_eaten["candy"] = 1
+snacks_eaten["popcorn"] += 3
+
+snacks_eaten["nachos"] += 4
+
+puts snacks_eaten
