a86d7bfd0c91d7b0fd802e77dc6c20c2e04d3eb6
spjesugq <jesus.gonzalez@simplepractice.com>
2025-08-01 12:03:00 -0600

    Add Mixin' It In flicks

---
+++ 26-mixin-it-in/flicks/lib/flicks/rankable.rb
@@ -0,0 +1,9 @@
+module Rankable
+  def thumbs_up
+    @rank += 1
+  end
+
+  def thumbs_down
+    @rank -= 1
+  end
+end
---
+++ 26-mixin-it-in/flicks/tests/flicks/all_tests.rb
@@ -0,0 +1,3 @@
+require "minitest/autorun"
+require_relative "movie_test"
+require_relative "playlist_test"
