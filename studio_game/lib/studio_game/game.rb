8652d6eb6d07c670e48ed21ef68fef2e96e87e9e
spjesugq <jesus.gonzalez@simplepractice.com>
2025-08-01 09:29:02 -0600

    Add File Ins and Outs studio_game

--- studio_game/lib/studio_game/game.rb
+++ studio_game/lib/studio_game/game.rb
@@ -8,6 +8,28 @@ class Game
     @players = []
   end
 
+  def load_players(from_file)
+    File.readlines(from_file, chomp: true).each do |line|
+      player, health = line.split(",")
+      add_player(Player.new(player, health.to_i))
+    end
+  end
+
+  def save_high_scores(to_file = "high_scores.txt")
+    File.open(to_file, "w") do |file|
+      file.puts "#{@title} High Scores:"
+      sorted_players.each do |player|
+        file.puts high_score_entry(player)
+      end
+    end
+  end
+
+  def high_score_entry(player)
+    name = player.name.ljust(20, ".")
+    score = player.score.round.to_s.rjust(5)
+    "#{name}#{score}"
+  end
+
   def add_player(player)
     @players << player
   end
@@ -32,9 +54,7 @@ class Game
 
     puts "\nHigh Scores:"
     sorted_players.each do |player|
-      name = player.name.ljust(20, ".")
-      points = player.score.round.to_s.rjust(5)
-      puts "#{name}#{points}"
+      puts high_score_entry(player)
     end
   end
 
--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -1,20 +1,9 @@
 require_relative "lib/studio_game/player"
 require_relative "lib/studio_game/game"
 
-player_1 = Player.new("finn", 60)
-player_2 = Player.new("lucy", 90)
-player_3 = Player.new("jase")
-player_4 = Player.new("alex", 125)
-players = [player_1, player_2, player_3, player_4]
-players.pop
-player_5 = Player.new("cole", 75)
-players.push(player_5)
-
-game = Game.new("Winner Takes All")
-game.add_player(player_1)
-game.add_player(player_2)
-game.add_player(player_3)
-game.add_player(player_4)
+game = Game.new("Guardians")
+players_file = File.join(__dir__, "players.csv")
+game.load_players(ARGV.shift || players_file)
 
 loop do
   puts "How many game rounds? ('quit' to 'exit')"
@@ -31,3 +20,4 @@ loop do
   puts ""
 end
 game.print_stats
+game.save_high_scores
