edf1c32ccdb402d96556398db8dd126c3a0bfa85
spjesugq <jesus.gonzalez@simplepractice.com>
2025-08-01 10:02:34 -0600

    Add Uh-Oh, Exception! studio_game

--- studio_game/lib/studio_game/player.rb
+++ studio_game/lib/studio_game/player.rb
@@ -34,7 +34,10 @@ class Player
 
   def self.from_csv(line)
     name, health = line.split(',')
-    Player.new(name, health.to_i)
+    Player.new(name, Integer(health))
+  rescue ArgumentError
+    puts "Ignored invalid health: #{health}"
+    Player.new(name)
   end
 
   def to_s
