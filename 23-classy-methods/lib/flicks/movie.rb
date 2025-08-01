e22a0001a18c28445371e3fdcb57f27f1e3895e0
spjesugq <jesus.gonzalez@simplepractice.com>
2025-08-01 09:34:16 -0600

    Add Classy Methods flicks

---
+++ 23-classy-methods/lib/flicks/movie.rb
@@ -0,0 +1,42 @@
+class Movie
+  attr_accessor :title, :snacks_eaten
+  attr_reader :rank
+
+  def initialize(title, rank = 5)
+    @title = title.capitalize
+    @rank = rank
+    @snacks_eaten = Hash.new(0)
+  end
+
+  def add_snack(name, price)
+    @snacks_eaten[name] += price
+  end
+
+  def thumbs_up
+    @rank += 1
+  end
+
+  def thumbs_down
+    @rank -= 1
+  end
+
+  def total_snack_price
+    @snacks_eaten.values.sum
+  end
+
+  def self.from_csv(line)
+    title, rank = line.split(",")
+    Movie.new(title, rank.to_i)
+  end
+
+  def to_csv
+    "#{@title},#{@rank}"
+  end
+
+  def to_s = "#{@title} has a rank of #{@rank}"
+end
+
+if __FILE__ == $0
+  movie = Movie.new("Star Wars", 10)
+  puts movie
+end
